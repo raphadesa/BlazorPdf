42dfb86ca8ce15d26cc334688cf9b2e789ba42ff
Raphaël Désalbres <raphael.desalbres@outlook.com>
2020-05-10 15:56:22 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorPdf/Controllers/GeneratePdfController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PdfGenerator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GeneratePdfController : ControllerBase 
+    {    
+        [HttpGet]
+        [Route("DownloadPdf")]
+        public IActionResult DownloadPdf()
+        {
+            var pdfGenerator = new BlazorPdf.GeneratePdf($"https://{Request.Host.Value}/");
+            var pdf = pdfGenerator.getPdf();
+            var pdfStream = new System.IO.MemoryStream();
+            pdfStream.Write(pdf, 0, pdf.Length);
+            pdfStream.Position = 0;
+            return new FileStreamResult(pdfStream, "application/pdf");
+        }
+    }
+}
---
+++ BlazorPdf/GeneratePdf.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Reflection;
+
+namespace BlazorPdf
+{
+    public class GeneratePdf
+    {
+        private string url { get; set; }
+        public GeneratePdf(string _url)
+        {
+            this.url = _url;
+        }
+
+        public byte[] getPdf()
+        {
+            var switches = $"-q {url} -";
+
+            string rotativaLocation = Path.Combine(Directory.GetCurrentDirectory(), "rotativa","wkhtmltopdf.exe");
+
+            using (var proc = new Process())
+            {
+                try
+                {
+                    proc.StartInfo = new ProcessStartInfo
+                    {
+                        FileName = rotativaLocation,
+                        Arguments = switches,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        RedirectStandardInput = true,
+                        CreateNoWindow = true
+                    };
+
+                    proc.Start();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+                using (var ms = new MemoryStream())
+                {
+                    proc.StandardOutput.BaseStream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
+
+
